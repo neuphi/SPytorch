1175c2ca2ef041393adf5eda282551c9aebd3db4
felix <mailfwagner@gmail.com>
2018-08-01 11:42:34 +0200

    draw function, nn update

---
+++ drawtorch.py
@@ -0,0 +1,128 @@
+#!/usr/bin/python3
+
+########## IMPORT LIBRARIES ###########################
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+
+########## LOAD DATABASE ##############################
+
+print ( "Now loading data" )
+import pickle
+f=open("data/data.pcl","rb")
+tr_data=pickle.load ( f )
+tr_labels=pickle.load ( f )
+val_data=pickle.load ( f )
+val_labels=pickle.load ( f )
+f.close()
+
+def Hash ( A ):           #define hash dictionary
+    return int(A[0]*10000.+A[1])
+X={}
+for d,l in zip (tr_data,tr_labels):
+    X[Hash(d)]=l
+
+##### CREATE MASS ARRAYS WITH LSP=300, #################
+##### DIFFERENT MOTHERS ################################
+
+masses = [] 
+for i in range ( 600, 1000, 10 ):
+    masses.append ( [ i, 300 ] )
+
+mass = torch.tensor( masses )
+
+##### Create some empty Arrays and stuff ##############
+
+pX, pY, pYm = [], [], []
+
+##### Load the model and make predictions ##############
+
+model = torch.load("data/torchmodel.h5")
+preds=model ( mass.type(torch.FloatTensor) )
+
+#### Create a tupel list with mass and predictions #####
+
+i=0
+for m,p in zip(mass,preds):
+    try:
+        pY.append ( X[Hash(m)] )
+        pX.append ( m[0].numpy() )
+        pYm.append ( p.item() )
+    except:
+        pass
+    i=i+1
+
+#### Test: Print lengths of the pY, pX, pYm #############
+#print (len(pY))
+#print (len(pX))
+#print (len(pYm))
+#print (i)
+
+#### Do the actual plot #################################
+
+plt.figure(0)
+plt.title(r'Fixed LSP, upperlimit vs mother', fontsize=20)
+plt.xlabel('mother')
+plt.ylabel('upper limit')
+plt.scatter ( pX, pY )
+plt.scatter ( pX, pYm, c='r' )
+plt.savefig ( "analysis/plots/plot.png" )
+
+#preds=model.predict ( mass )
+#print ( "Now predict" )
+#for m,p in zip ( mass,preds ):
+#    print ( "%s -> %s, %s" % ( m,p[0], X[hash(m)] ) )
+
+#######CREATE A HEATMAP #################################
+
+### initialize the emphy arrays
+Heat_calc = [[0 for i in range(40)] for j in range(40)] 
+Heat_pred = [[0 for i in range(40)] for j in range(40)]
+
+### fill the arrays with data
+i = 0
+for mother in range ( 600, 1000, 10 ):
+    Hmasses = []
+    Hpreds = []
+    for lsp in range (0, 400, 10 ):
+        Hmasses.append ( [ mother, lsp ] )
+    Hmass = torch.Tensor( Hmasses )
+    Hpreds=model( Hmass )
+    j=0    
+    for m in Hmass:
+        try:
+            Heat_calc[i][j] = X[Hash(m)]    #X[Hash(m)] gives the upperlimit for the masses
+            Heat_pred[i][j] = Hpreds[j][0].item()            #p is the predictions
+        except:
+            Heat_calc[i][j] = 0    #X[Hash(m)] gives the upperlimit for the masses
+            Heat_pred[i][j] = 0            #p is the predictions
+        j+=1
+    i+=1
+
+### calculate the quadratic error
+Heat_diff = (np.array(Heat_pred) - np.array(Heat_calc))#**2 
+
+###the three heatmaps
+plt.title(r'Calculated Upper Limits', fontsize=20)
+plt.xlabel('lsp 0-400')
+plt.ylabel('mother 1000-600')
+plt.imshow(Heat_calc)       
+plt.savefig("analysis/plots/heatcalc.png")
+plt.title(r'Predicted Upper Limits', fontsize=20)
+plt.imshow(Heat_pred)
+plt.savefig("analysis/plots/heatpred.png")
+plt.title(r'Quadrativc Difference Prediction/Calculation', fontsize=20)
+plt.imshow(Heat_diff)
+plt.savefig("analysis/plots/heatdiff.png")
+
+###the curves next to each other for one lsp(slice)
+plt.figure(1)         
+plt.title(r'Slice of the Heatmap', fontsize=20)
+plt.xlabel('lsp')
+plt.ylabel('upper limit')
+plt.plot([i*10 for i in range(40)], Heat_calc[20])
+plt.plot([i*10 for i in range(40)], Heat_pred[20], c = 'r')
+plt.plot([i*10 for i in range(40)], Heat_diff[20], c = 'g')
+plt.savefig("analysis/plots/slice.png")
+
--- traintorch.py
+++ traintorch.py
@@ -2,29 +2,35 @@
 
 # -*- coding: utf-8 -*-
 
+####################### IMPORT STUFF ############################
+
 import torch
 import numpy as np
 
-# N is batch size; D_in is input dimension;
-# H is hidden dimension; D_out is output dimension.
+###################### DEFINE CLASS ##############################
 
-N, D_in, H, D_out = 5050, 2, 8, 2
+class TwoLayerNet(torch.nn.Module):
+    def __init__(self, D_in, H, D_out):
+        """
+        In the constructor we instantiate two nn.Linear modules and assign them as
+        member variables.
+        """
+        super(TwoLayerNet, self).__init__()
+        self.linear1 = torch.nn.Linear(D_in, H)
+        self.linear2 = torch.nn.Linear(H, D_out)
 
-# Create random Tensors to hold inputs and outputs
-
-model = torch.nn.Sequential(
-    torch.nn.Linear(D_in, H),
-    torch.nn.ReLU(),
-	torch.nn.ReLU(),
-	torch.nn.ReLU(),
-    torch.nn.Linear(H, D_out),
-)
-loss_fn = torch.nn.MSELoss(size_average=False)
-
-learning_rate = 1e-4
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    def forward(self, x):
+        """
+        In the forward function we accept a Tensor of input data and we must return
+        a Tensor of output data. We can use Modules defined in the constructor as
+        well as arbitrary operators on Tensors.
+        """
+        h_relu = self.linear1(x).clamp(min=0)
+        y_pred = self.linear2(h_relu)
+        return y_pred
 
 
+######################## LOAD DATA ################################
 
 print ( "Now loading data" )
 import pickle
@@ -35,52 +41,77 @@ val_data=pickle.load ( f )
 val_labels=pickle.load ( f )
 f.close()
 
-len_tr_x = len(tr_data)
-len_tr_y = len(tr_data[0])
-len_val_x = len(val_data)
-len_val_y = len(val_data[0])
+def hash ( A ):          #define a hash dictionary
+    return int(A[0]*10000.+A[1])
 
-tr_data_torch      = torch.zeros(len_tr_x, len_tr_y)
-tr_labels_torch    = torch.zeros(len_tr_x, len_tr_y)
-val_data_torch     = torch.zeros(len_val_x, len_val_y)
-val_labels_torch   = torch.zeros(len_val_x, len_val_y)
+X={}
+for d,l in zip (tr_data,tr_labels):
+    X[hash(d)]=l
 
-for i in range(len_tr_x):
+###################### INITIALIZE NN ############################
+
+# N is batch size; D_in is input dimension;
+# H is hidden dimension; D_out is output dimension.
+
+N, D_in, H, D_out, N_val = len(tr_data), 2, 8, 1, len(val_data)
+
+model = TwoLayerNet(D_in, H, D_out)
+
+#model = torch.nn.Sequential(
+#    torch.nn.Linear(D_in, H),
+#    torch.nn.ReLU(),
+#    torch.nn.Linear(H, D_out),
+#)
+#loss_fn = torch.nn.MSELoss(size_average=False)
+
+learning_rate = 1e-4
+criterion = torch.nn.MSELoss(size_average=False)
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+#optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+###################### MODIFY DATA FOR NN #########################
+
+tr_data_torch      = torch.zeros(N, D_in)
+tr_labels_torch    = torch.zeros(N, D_out)
+val_data_torch     = torch.zeros(N_val, D_in)
+val_labels_torch   = torch.zeros(N_val,  D_out)
+
+for i in range(N):
 	tr_data_torch[i]   = torch.from_numpy(tr_data[i])
 	tr_labels_torch[i] = tr_labels[i]
 
-for i in range(len_val_x):
+for i in range(N_val):
 	val_data_torch[i]   = torch.from_numpy(val_data[i])
 	val_labels_torch[i] = val_labels[i]
 
-
-#def hash ( A ):
-#    return int(A[0]*10000.+A[1])
-
-#X={}
-#for d,l in zip (tr_data,tr_labels):
-#    X[hash(d)]=l
-
+###################### FIT NN #####################################
 
 print ( "Now fitting ... " )
-for t in range(1000):
-
+for t in range(500):
     labels_pred = model(tr_data_torch)
 
-    loss = loss_fn(labels_pred, tr_labels_torch)
+    loss = criterion(labels_pred, tr_labels_torch)
+    #loss = loss_fn(labels_pred, tr_labels_torch)
     print(t, loss.item())
 
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
 
+print ( "Done fitting" )
 
-#print ( "Done fitting" )
-#mass = np.array( [ [ 600, 200 ], [ 700, 200 ], [ 800, 200 ], [ 900, 200 ], [ 1000, 200 ] ] )
-#preds=model.predict ( mass )
-#print ( "Now predict" ) 
-#for m,p in zip ( mass,preds ):
-#    print ( "%s -> %s, %s" % ( m,p[0], X[hash(m)] ) )
+################# MAKE EXAMPLE PREDICTIONS ########################
+mass = torch.tensor( [ [ 600, 200 ], [ 700, 200 ], [ 800, 200 ], [ 900, 200 ], [ 1000, 200 ] ] )
+preds=model( mass.type(torch.FloatTensor) )
+print ( "Now predict" ) 
 
-#model.save("data/model.h5")
-#model.save_weights("data/weights.h5")
+for m,p in zip ( mass,preds ):
+    print ( "%s -> %s, %s" % ( m,p, X[hash(m)]) )
+
+################ SAVE MODEL ##################################
+#The second saves and loads the entire model:
+#torch.save(the_model, PATH)
+#Then later:
+#the_model = torch.load(PATH)
+
+torch.save(model,"data/torchmodel.h5")
