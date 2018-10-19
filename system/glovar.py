7ef1dba1d13eb6d5e95d0a2dc7685a6ccb0248ae
FelixWagner18 <mailfwagner@gmail.com>
2018-10-19 19:53:04 +0200

    device error

--- system/glovar.py
+++ system/glovar.py
@@ -12,7 +12,7 @@ parser.add_argument('--disable-cuda', action='store_true',
 args = parser.parse_args()
 args.device = None
 if not args.disable_cuda and torch.cuda.is_available():
-    devicenmbr = ("Which GPU to use?")
+    devicenmbr = input("Which GPU to use?")
     args.device = torch.device('cuda:'+devicenmbr)
 else:
     args.device = torch.device('cpu')
